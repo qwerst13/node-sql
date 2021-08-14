5c7148b52c6bbed798760b5770771caa25e3066b
biohazard <qwerst13@gmail.com>
2021-08-14 22:05:34 +0300

    CRUD responses

---
+++ routes/todo.js
@@ -0,0 +1,20 @@
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", (req, res) => {
+
+});
+
+router.post("/", (req, res) => {
+
+});
+
+router.put("/:id", (req, res) => {
+
+});
+
+router.delete("/:id", (req, res) => {
+
+});
+
+module.exports = router;
